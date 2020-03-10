066df4f7e1a2170f24f11c195d2bcf0391ee790e
ngocpt <noahphan90@gmail.com>
2020-03-10 17:43:07 +0700

    Standardize the code following the course

--- data-structures-algorithms/src/main/java/com/hatake/ude/sort/CountingSort.java
+++ data-structures-algorithms/src/main/java/com/hatake/ude/sort/CountingSort.java
@@ -2,15 +2,16 @@ package com.hatake.ude.sort;
 
 public class CountingSort {
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        int[] intArray = { 2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
-        //int[] intArray = {-5, -10, 0, -3, 8, 5, -1, 10};
+       int[] intArray = { 2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+       int[] intArray2 = {-5, -10, 0, -3, 8, 5, -1, 10};
 
-        countingSort(intArray, -5, 10);
-        System.out.printf("Millis: %d" , System.currentTimeMillis() - start);
+        countingSort(intArray, 2, 10);
+        System.out.println();
+        countingSort(intArray2, -10, 10);
     }
 
     public static void countingSort(int[] input, int min, int max) {
+        long start = System.currentTimeMillis();
         int[] countArray = new int[max - min + 1];
 
         for (int i = 0; i < input.length; i++) {
@@ -18,16 +19,25 @@ public class CountingSort {
         }
 
         int j = 0;
-        for (int i = 0; i < countArray.length; i ++) {
-           while (countArray[i] > 0) {
-               input[j] = i + min;
-               j++;
-               countArray[i]--;
+        for (int i = min; i <= max; i ++) {
+           while (countArray[i - min] > 0) {
+               input[j++] = i;
+               countArray[i - min]--;
            }
         }
+
+        // MY OWN WAY
+        /*for (int i = 0; i < countArray.length; i ++) {
+            while (countArray[i] > 0) {
+                input[j++] = i + min;
+                countArray[i]--;
+            }
+        }*/
 
         for (int i : input) {
            System.out.println(i);
         }
+
+        System.out.printf("Millis: %d" , System.currentTimeMillis() - start);
     }
 }
