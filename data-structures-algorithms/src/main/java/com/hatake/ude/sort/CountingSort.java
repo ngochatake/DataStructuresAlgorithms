a8e48de378df34eca82726684e33eb3a37ccc399
ngocpt <noahphan90@gmail.com>
2020-03-10 16:46:40 +0700

    It works perfectly. Although, the algorithm is different than the course

--- data-structures-algorithms/src/main/java/com/hatake/ude/sort/CountingSort.java
+++ data-structures-algorithms/src/main/java/com/hatake/ude/sort/CountingSort.java
@@ -4,27 +4,30 @@ public class CountingSort {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         int[] intArray = { 2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
+        //int[] intArray = {-5, -10, 0, -3, 8, 5, -1, 10};
 
-        int[] temp = new int[intArray.length + 1]; // probably length of array = max number
-        int[] sortedArray = new int[intArray.length + 1];
-        for (int i : intArray) {
-           temp[i] = temp[i] + 1;
+        countingSort(intArray, -5, 10);
+        System.out.printf("Millis: %d" , System.currentTimeMillis() - start);
+    }
+
+    public static void countingSort(int[] input, int min, int max) {
+        int[] countArray = new int[max - min + 1];
+
+        for (int i = 0; i < input.length; i++) {
+            countArray[input[i] - min]++;
         }
 
-       for (int i = 0; i < temp.length; i ++) {
-          int tempValue = temp[i];
-          int k = 0;
-          while (tempValue > 0) {
-              sortedArray[i + k] = i;
-              tempValue--;
-              k++;
-          }
-       }
+        int j = 0;
+        for (int i = 0; i < countArray.length; i ++) {
+           while (countArray[i] > 0) {
+               input[j] = i + min;
+               j++;
+               countArray[i]--;
+           }
+        }
 
-        for (int i : sortedArray) {
+        for (int i : input) {
            System.out.println(i);
         }
-
-        System.out.printf("Millis: %d" , System.currentTimeMillis() - start);
     }
 }
